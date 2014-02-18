9190cbb37753dde921798c81d8a8e098f931b9c9
raul <raul@zamacona.me>
2014-02-17 21:15:53 -0600

    estilos y base y logo

--- js/etimex.js
+++ js/etimex.js
@@ -1,18 +1,24 @@
-$(document).ready(function(e) {
-    $("#otro").hide();    
-	$(".primer_campo").focus(); 
-	activarCampoOtro();   
-});
+(function($){
 
+	"use strict";
 
-// Funciones
-function activarCampoOtro() {
-	$("input[type=radio]").click(function(){
-		if($(this).attr("value") == "otro") {
-			$("#otro").show();	
-			$("#otro input").focus();
-		} else {
-			$("#otro").hide();	
-		}
-	});	
-}
+	$(function(){
+
+		$(".primer_campo").focus();
+		activarCampoOtro();
+
+	});
+
+	// Funciones
+	function activarCampoOtro() {
+		$("input[type=radio]").click(function(){
+			if($(this).attr("value") == "otro") {
+				$("#otro").show();
+				$("#otro input").focus();
+			} else {
+				$("#otro").hide();
+			}
+		});
+	}
+
+})(jQuery);
