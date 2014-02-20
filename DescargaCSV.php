44d73526446d680e14a3d382409b020782571fd1
maichomper <maichomper@gmail.com>
2014-02-20 01:56:08 -0600

    login y exportacion csv

---
+++ DescargaCSV.php
@@ -0,0 +1,32 @@
+<?php
+	// Conectar con bd
+	$con=mysqli_connect("db515467882.db.1and1.com","dbo515467882","Registro@123","db515467882");
+	if (mysqli_connect_errno()){
+	  echo "Error, no se pudo conectar la base de datos: " . mysqli_connect_error();
+	} 
+
+
+	$archivo = 'registrados_etimex.csv';
+	$encabezado = array('Nombre', 'Apellidos', 'Email');
+	 
+	$handle = fopen($archivo, 'w');
+	fputcsv($handle, $encabezado, ',', '"');
+	 
+	$sql = mysqli_query($con, 'SELECT * FROM TB_Usuario');
+	 
+	while($results = mysqli_fetch_array($sql)) {
+		$row = array(			
+			$results[1],
+			$results[2],
+			$results[3] 
+		);
+		fputcsv($handle, $row, ',', '"');
+	}
+	 
+	fclose($handle);
+	
+	header('Location: '.$archivo);
+
+	// Desconectar bd
+	mysqli_close($con);
+?>
--- js/etimex.js
+++ js/etimex.js
@@ -20,5 +20,9 @@
 			}
 		});
 	}
+	
+	
+	function getURLParameter(name) {
+  		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null}
 
 })(jQuery);
