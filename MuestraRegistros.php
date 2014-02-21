4e9e3c35637519e999d91a0f970c045b0777b066
raul <raul@zamacona.me>
2014-02-21 00:31:58 -0600

    consulta

--- MuestraRegistros.php
+++ MuestraRegistros.php
@@ -7,28 +7,26 @@
 	$query="SELECT F_Nombre, F_ApePat, F_ApeMat, F_Empresa, F_Telefono, F_Celular, F_Correo FROM TB_Usuario U INNER JOIN TB_Laboral L ON U.F_IdUsuario = L.F_IdUsuario";
 	$registros=mysqli_query($con, $query );
 
-	echo "<table border='1' >
-	<tr>
-	<th class='span c-2' align=center><b>Nombre</b></th>
-	<th class='span c-2' align=center><b>Apellido Paterno</b></th>
-	<th class='span c-2' align=center><b>Empresa</b></th>
-	<th class='span c-2' align=center><b>Tel.</b></th>
-	<th class='span c-2' align=center><b>Cel.</b></th>
-	<th class='span c-2' align=center><b>Correo</b></th></th>";
+	echo "<div class='width clearfix table columna c-12'><div class='renglon clearfix'>
+	<div class='span c-2 small-4 text-center'>Nombre</div>
+	<div class='span c-2 small-4 text-center'>Apellido Paterno</div>
+	<div class='span c-2 small-4 text-center'>Empresa</div>
+	<div class='span c-2 small-4 text-center'>Tel.</div>
+	<div class='span c-2 small-4 text-center'>Cel.</div>
+	<div class='span c-2 small-4 text-center'>Correo</div></div>";
 
 	while($data = mysqli_fetch_array($registros))
 	{
-		echo "<tr>";
-		echo "<td align=center>$data[0]</td>";
-		echo "<td align=center>$data[1]</td>";
-		echo "<td align=center>$data[2]</td>";
-		echo "<td align=center>$data[3]</td>";
-		echo "<td align=center>$data[4]</td>";
-		echo "<td align=center>$data[5]</td>";
-		echo "<td align=center>$data[6]</td>";
-		echo "</tr>";
+		echo "<div class='renglon clearfix'>";
+		echo "<div class='span c-2 small-4'>$data[0]</div>";
+		echo "<div class='span c-2 small-4'>$data[1]</div>";
+		echo "<div class='span c-2 small-4'>$data[3]</div>";
+		echo "<div class='span c-2 small-4'>$data[4]</div>";
+		echo "<div class='span c-2 small-4'>$data[5]</div>";
+		echo "<div class='span c-2 small-4'>$data[6]</div>";
+		echo "</div>";
 	}
-	echo "</table>";
+	echo "</div>";
 
 	// Desconectar bd
 	mysqli_close($con);
